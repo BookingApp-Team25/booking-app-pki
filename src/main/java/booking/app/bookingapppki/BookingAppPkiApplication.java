e35ff574383e8b4fdbacc073c1f63c2df0b6bb05
Miroslav0222 <105170901+Miroslav0222@users.noreply.github.com>
2024-04-19 10:55:53 +0200

    Initial spring setup

---
+++ src/main/java/booking/app/bookingapppki/BookingAppPkiApplication.java
@@ -0,0 +1,13 @@
+package booking.app.bookingapppki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppPkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAppPkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/booking/app/bookingapppki/BookingAppPkiApplicationTests.java
@@ -0,0 +1,13 @@
+package booking.app.bookingapppki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppPkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
